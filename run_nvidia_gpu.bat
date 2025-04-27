fb82571d36baca03988bb1d747af5d9488c2c5aa
zhaozigu <zhaozigunan@163.com>
2025-04-27 15:43:54 +0800

    feat: use uv as package manager

--- run_nvidia_gpu.bat
+++ run_nvidia_gpu.bat
@@ -1,2 +1,6 @@
-pipenv run python main.py --listen 0.0.0.0 --minio-endpoint 127.0.0.1:5200 --minio-access-key "R1pgTvwrHGwO60yOxkQd" --minio-secret-key "cWuHVgYpMswGioVnuPAXaaOo73uLtqD3Doa8KITH"
+@echo off
+REM 激活 uv 创建的虚拟环境
+call .venv\Scripts\activate
+
+python main.py --listen 0.0.0.0 --minio-endpoint 127.0.0.1:5200 --minio-access-key "R1pgTvwrHGwO60yOxkQd" --minio-secret-key "cWuHVgYpMswGioVnuPAXaaOo73uLtqD3Doa8KITH"
 pause
